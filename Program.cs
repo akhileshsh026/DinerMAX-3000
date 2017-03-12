452e891b5bde537dbf448392e20bdc81dee0b3fa
AKHILESH <akhileshsh026@users.noreply.github.com>
2017-03-12 16:55:29 +0530

    Projects files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMAX_3000_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu SummerMenu = new Menu();
+            SummerMenu.Name = "Summer Menu";
+            MenuItem salmon = new MenuItem();
+            salmon.title = "Salmoon";
+            salmon.Description = " freash chease";
+            salmon.Price = 123;
+
+            SummerMenu.items.Add(salmon);
+        }
+    }
+}
