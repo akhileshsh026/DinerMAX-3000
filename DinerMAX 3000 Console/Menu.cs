b21c8c8379b5faae56f91a395236ec65b8e031ff
Akhileshsh026@hotmail.com <Akhilesh@123>
2017-03-12 17:07:12 +0530

    created new method under which they are added items

--- DinerMAX 3000 Console/Menu.cs
+++ DinerMAX 3000 Console/Menu.cs
@@ -15,16 +15,28 @@ namespace DinerMAX_3000_Console
             items = new List<MenuItem>();
         }
 
+        // 4 modified in the main f(x) creating the obj .
 
-        // menu sholud have a name 
+            //5 
+
+            public void AddMenuItem(String Title , double Price , String Description)
+        {
+            MenuItem item = new MenuItem();
+            item.title = "Title ";
+            item.Description = "Description";
+            item.Price = 234.56;
+            items.Add(item);
+        }
+
+        //1 menu sholud have a name 
 
         public String Name;
 
-        // contains items list can hold an item of any item 
+        //2 contains items list can hold an item of any item 
 
         public List<MenuItem> items;
 
-        // to use items we need to initilize the items of list as soon as the menu object created 
+        //3 to use items we need to initilize the items of list as soon as the menu object created 
 
     }
 }
--- DinerMAX 3000 Console/Program.cs
+++ DinerMAX 3000 Console/Program.cs
@@ -12,12 +12,17 @@ namespace DinerMAX_3000_Console
         {
             Menu SummerMenu = new Menu();
             SummerMenu.Name = "Summer Menu";
-            MenuItem salmon = new MenuItem();
-            salmon.title = "Salmoon";
-            salmon.Description = " freash chease";
-            salmon.Price = 123;
+            SummerMenu.AddMenuItem("Salmoon", 231, "freash chease");   // Step 6 add more and more items
+            SummerMenu.AddMenuItem("Sharbad", 431, "Could Drink");
+            SummerMenu.AddMenuItem("Papaya", 531, "Juice");
+            SummerMenu.AddMenuItem("Mango", 2731, "Pana"); 
 
-            SummerMenu.items.Add(salmon);
+            // Step 4
+            //MenuItem salmon = new MenuItem();
+            //salmon.title = "Salmoon";
+            //salmon.Description = " freash chease";
+            //salmon.Price = 123;
+            //SummerMenu.items.Add(salmon);
         }
     }
 }
